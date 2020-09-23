513ec139efab625351b3d1afadea7bfe2cad156f
Jinay Shah <jinayshah86@gmail.com>
2020-09-23 19:19:11 +0530

    Documentation

--- pgb/add.py
+++ pgb/add.py
@@ -1,2 +1,12 @@
 def add(a: int, b: int) -> int:
+    """
+    Adds two number
+
+    Args:
+        a (int): First argument
+        b (int): Second argument
+
+    Returns:
+        Sum of the two arguments
+    """
     return a + b
--- pgb/mul.py
+++ pgb/mul.py
@@ -1,2 +1,12 @@
 def mul(a: int, b: int) -> int:
+    """
+    Multiplies two number
+
+    Args:
+        a (int): First argument
+        b (int): Second argument
+
+    Returns:
+        Product of the two arguments
+    """
     return a * b
