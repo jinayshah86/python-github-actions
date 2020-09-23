bafbecc86aa7dc918daef6a341e8a97c84b56cb0
Jinay Shah <jinayshah86@gmail.com>
2020-09-23 19:27:28 +0530

    Add subtract function

---
+++ pgb/sub.py
@@ -0,0 +1,12 @@
+def sub(a: int, b: int) -> int:
+    """
+    Subtracts two number
+
+    Args:
+        a (int): First argument
+        b (int): Second argument
+
+    Returns:
+        Difference of the two arguments
+    """
+    return a - b
