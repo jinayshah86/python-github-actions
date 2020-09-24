efcd2ad1d546c1dd4ce643b7926521247422f47d
Jinay Shah <jinayshah86@gmail.com>
2020-09-24 14:06:28 +0530

    Test 22

---
+++ pgb/pow4.py
@@ -0,0 +1,14 @@
+from pgb import pow3
+import os
+def pow(a: int, b: int) -> int:
+    """
+    Power of two number
+
+    Args:
+        a (somethibng): First argument
+        b (int): Second argument
+
+    Returns:
+        Power of second arguments on the base of first argument
+    """
+    return a ** b
